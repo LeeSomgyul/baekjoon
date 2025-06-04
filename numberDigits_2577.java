1921992ecbf1dc2b51a99e26c8ce01cff50f06bd
LeeSomgyul <rmawl8600@naver.com>
2025-06-04 11:59:07 +0900

    20250604
    2577. 숫자 개수/
    2920. 음계

---
+++ numberDigits_2577.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class numberDigits_2577 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int A = scan.nextInt();
+        int B = scan.nextInt();
+        int C = scan.nextInt();
+
+        int M = A * B * C;
+
+        String str = Integer.toString(M);
+        int[] arr = new int[10];
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            int digit = c - '0';
+            arr[digit]++;
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(arr[i]);
+        }
+
+        scan.close();
+    }
+}
--- scale_2920.java
+++ scale_2920.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Main {
+public class scale_2920 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
